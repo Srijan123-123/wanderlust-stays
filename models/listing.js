746f270e75ab7fdb994df388a8dfe2026db50452
Srijan Singh <srijansingh083@gmail.com>
2026-02-28 18:30:21 +0530

    Day 3: Fixed image rendering bug and implemented client & server-side validations

--- models/listing.js
+++ models/listing.js
@@ -10,10 +10,13 @@ const listingSchema = new Schema({
     image: {
         filename: String,
         url: {
-            type: String,
-            default: "https://www.istockphoto.com/photo/sunset-on-tropical-beach-gm534716373-57062266?utm_source=unsplash&utm_medium=affiliate&utm_campaign=srp_photos_top&utm_content=https%3A%2F%2Funsplash.com%2Fs%2Fphotos%2Fa-beach-photo-with-sunset&utm_term=a+beach+photo+with+sunset%3A%3Awallpapers-no-affiliates%3Aexperiment%3A10b375a3-d4c5-4dc4-b224-9d3f7e953eee",
-            set: (v) => v === "" ? "https://www.istockphoto.com/photo/sunset-on-tropical-beach-gm534716373-57062266?utm_source=unsplash&utm_medium=affiliate&utm_campaign=srp_photos_top&utm_content=https%3A%2F%2Funsplash.com%2Fs%2Fphotos%2Fa-beach-photo-with-sunset&utm_term=a+beach+photo+with+sunset%3A%3Awallpapers-no-affiliates%3Aexperiment%3A10b375a3-d4c5-4dc4-b224-9d3f7e953eee" : v,
-        },
+  type: String,
+  default: "https://images.unsplash.com/photo-1507525428034-b723cf961d3e",
+  set: (v) =>
+    v === ""
+      ? "https://images.unsplash.com/photo-1507525428034-b723cf961d3e"
+      : v,
+},
 
         
     },
